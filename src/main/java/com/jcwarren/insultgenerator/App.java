0915039664efb00978c6080f0157840b189584c8
Scott Serbousek <airetamstrm@gmail.com>
2019-08-07 20:30:24 -0400

    Jerks suck

--- src/main/java/com/jcwarren/insultgenerator/App.java
+++ src/main/java/com/jcwarren/insultgenerator/App.java
@@ -2,6 +2,6 @@ package com.jcwarren.insultgenerator;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        System.out.println("Hello Jerk");
     }
 }
